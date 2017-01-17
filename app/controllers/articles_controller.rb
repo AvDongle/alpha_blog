b6a21624a5d580125adf0e3e8049b8ba9aa76f44
Cliffery <cliff@curvetomorrow.com.au>
2017-01-17 17:11:44 +1100

    mostly working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
-    
+
   end
 
   def edit
@@ -36,6 +36,13 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+    flash[:notice] = "Article was kuputski"
   end
 
   private
