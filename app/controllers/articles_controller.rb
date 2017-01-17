074474869b4398751978261e0e7527ef1c2c73ce
Cliffery <cliff@curvetomorrow.com.au>
2017-01-17 16:43:07 +1100

    add edit, update show & index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,15 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def index
+    @articles = Article.all
+    
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     #render plain:params[:article].inspect
     @article = Article.new(article_params)
@@ -12,6 +21,16 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article)
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article successfully edited"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
 
