7336d2405f4008d730ee3e451bf491e28ffb5d3c
Cliffery <cliff@curvetomorrow.com.au>
2017-01-18 16:29:23 +1100

    install bootstrap

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,3 @@
 class Article < ActiveRecord::Base
-  validates :title,:description, presence: true, length: {minimum: 3, maximum: 50}
+  validates :title,:description, presence: true, length: {minimum: 3, maximum: 500}
 end
