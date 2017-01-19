fb8eda37a6d85f9d9b91a01708d9487b6bcb5023
Cliffery <cliff@curvetomorrow.com.au>
2017-01-19 15:33:39 +1100

    catrosel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
-class PagesController < ApplicationController
-  def home
+require 'open-uri'
+require 'json'
 
+class PagesController < ApplicationController
+
+  def home
+    json = JSON.parse(open("http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC").read)
+    @num = json['data'].count
+    @cliff = Array.new
+    for i in 0..4
+      @cliff.append(json['data'][rand(@num)]['images']['fixed_height']['url'])
+    end
   end
 
   def about
