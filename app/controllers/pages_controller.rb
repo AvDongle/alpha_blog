db05524e583bd2edc0dc0ec6dafd35f43c599c9c
Cliffery <cliff@curvetomorrow.com.au>
2017-01-20 16:06:05 +1100

    styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,11 @@ require 'json'
 class PagesController < ApplicationController
 
   def home
-    json = JSON.parse(open("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC").read)
-    @num = json['data'].count
+    #json = JSON.parse(open("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC").read)
     @cliff = Array.new
     for i in 0..4
-      @cliff.append(json['data'][rand(@num)]['images']['fixed_height']['url'])
+      json = JSON.parse(open("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC").read)
+      @cliff.append(json['data']['image_url'])#['fixed_height']['url'])
     end
   end
 
